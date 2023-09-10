62c9da15f7f9fd2c2bcd6ef63238aaf6675199fa
Abdullah Bilal <abdullah.bilal@takafulbazaar.pk>
2023-09-10 22:31:53 +0500

    first commit

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+  constructor(public messagesService: MessagesService) {}
+  @Get()
+  async listMessages() {
+    const messages = await this.messagesService.findAll();
+    if (!messages) {
+      throw new NotFoundException('Messages not found');
+    }
+    return messages;
+  }
+
+  @Post()
+  createMessages(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
+  }
+
+  @Get('/:id')
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+    if (!message) {
+      throw new NotFoundException('Message not found');
+    }
+    return message;
+  }
+}
